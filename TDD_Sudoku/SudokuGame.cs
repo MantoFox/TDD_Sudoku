47b82f9b4262cdf53a22ccb403f5a191eafc6bee
KuroKaraage <MantoFox@users.noreply.github.com>
2023-09-18 00:20:27 +0900

    [Implement] GenerateBoard 慢慢填入數字, 且每次填完數字就去檢查數值是否合法, 直到填滿所有格子

--- TDD_Sudoku/SudokuGame.cs
+++ TDD_Sudoku/SudokuGame.cs
@@ -1,20 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 internal class SudokuGame
 {
+    internal int[,] board = new int[9, 9];
+
     public SudokuGame()
     {
     }
 
     internal void Play()
     {
-        PrintBoard();
+        var temp = GenerateBoard();
+        if (temp)
+        {
+            PrintBoard();
+        }
     }
 
     internal void PrintBoard()
     {
-        int[,] board = new int[9, 9];
-
-        for (int row = 0; row < 9; row++)
+        for (var row = 0; row < 9; row++)
         {
             for (int col = 0; col < 9; col++)
             {
@@ -22,5 +26,61 @@ internal class SudokuGame
             }
             Console.WriteLine();
         }
+    }
+
+    private bool GenerateBoard()
+    {
+        for (var row = 0; row < 9; row++)
+        {
+            for (var col = 0; col < 9; col++)
+            {
+                if (board[row, col] == 0)
+                {
+                    for (var num = 1; num <= 9; num++)
+                    {
+                        if (IsValid(row, col, num))
+                        {
+                            board[row, col] = num;
+
+                            if (GenerateBoard())
+                            {
+                                return true;
+                            }
+
+                            board[row, col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private bool IsValid(int row, int col, int num)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[row, i] == num || board[i, col] == num)
+            {
+                return false;
+            }
+        }
+
+        int boxStartRow = row - row % 3;
+        int boxStartCol = col - col % 3;
+
+        for (int i = boxStartRow; i < boxStartRow + 3; i++)
+        {
+            for (int j = boxStartCol; j < boxStartCol + 3; j++)
+            {
+                if (board[i, j] == num)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
